5e0b5727fa5d332f608f76a05d121e7803663ab1
depointhomepage <web3mario@proton.me>
2024-10-24 12:09:39 +0800

    update

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -76,7 +76,7 @@ export const Home = () => {
 
         init();
     }, [client, initData, joinStatus]);
-    console.log(userInfo)
+
     return userInfo.status && !isLoading ? <div className='flex flex-col w-full justify-center text-lg'>
         {/* <div className="flex justify-center h-24 bg-[url('./assets/images/logo_with_text.png')] bg-no-repeat bg-cover"> */}
         <NavLink to="/leaders">
