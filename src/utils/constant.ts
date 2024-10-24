4c975990399afdd4dca42568fb7fc333ed9618bd
depointhomepage <web3mario@proton.me>
2024-10-24 17:27:10 +0800

    update

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,5 +1,5 @@
 export const FactoryAddr = "EQB_xLfZPt4T1xtHYSVr6hrpt6evIk-Thty9i-D-gss4w5YN";
 export const defaultReferral = "0";
 export const defaultPhotoURL = "https://www.gravatar.com/avatar/?d=identicon";
-export const defaultChatID = "-1002368471950";
-export const defaultInviteLink = "https://t.me/telehunterog";
+export const defaultChatID = "-1002297129889";
+export const defaultInviteLink = "https://t.me/thtestgroup2";
