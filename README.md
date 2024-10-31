27ba9233f0dee3d030a0ddffbc86cf2126359ab0
depointhomepage <web3mario@proton.me>
2024-10-31 18:05:59 +0800

    update

---
+++ README.md
@@ -0,0 +1,4 @@
+1. npm run build
+2. git add .
+3. git commit -m 'update'
+4. git push
--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -2,4 +2,4 @@ export const FactoryAddr = "EQB_xLfZPt4T1xtHYSVr6hrpt6evIk-Thty9i-D-gss4w5YN";
 export const defaultReferral = "0";
 export const defaultPhotoURL = "https://www.gravatar.com/avatar/?d=identicon";
 export const defaultChatID = "-1002238168504";
-export const defaultInviteLink = "https://t.me/telehunterxyz";
+export const defaultInviteLink = "https://t.me/telehunterxyz/";
